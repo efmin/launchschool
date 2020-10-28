d1c82166013d06bc765ddd87476429f15e79d00c
EFFY MIN <effyverse@gmail.com>
2020-10-28 17:14:48 -0400

    use HEREDOC to format output for operator prompt

--- rb101/lesson_2/calculator.rb
+++ rb101/lesson_2/calculator.rb
@@ -9,36 +9,51 @@ end
 
 prompt("Welcome to Calculator!")
 
-number1 = nil
-number2 = nil
-
 loop do
-  prompt("What's the first number?")
-  number1 = Kernel.gets().chomp()
-  break if valid_num?(number1)  
-  prompt("Invalid number.")
+  number1 = nil
+  number2 = nil
+
+  loop do
+    prompt("What's the first number?")
+    number1 = Kernel.gets().chomp()
+    break if valid_num?(number1)  
+    prompt("Invalid number.")
+  end
+
+  loop do
+    prompt("What's the second number?")
+    number2 = Kernel.gets().chomp()
+    break if valid_num?(number2)  
+    prompt("Invalid number.")
+  end
+
+  operator_prompt = <<-MSG
+  What operation would you like to perform?
+  1) add
+  2) subtract
+  3) multiply
+  4) divide
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = Kernel.gets().chomp()
+
+  result = case operator
+  when '1'
+    number1.to_i() + number2.to_i()
+  when '2'  
+    number1.to_i() - number2.to_i()
+  when '3'
+    number1.to_i() * number2.to_i()
+  when '4'
+    number1.to_f() / number2.to_f()
+  end
+
+  prompt("The result is #{result}.")
+  prompt("Would you like to perform another calculation? (Y to calculate again)")
+  repeat = gets.chomp.downcase
+  break unless repeat == "y"
 end
 
-loop do
-  prompt("What's the second number?")
-  number2 = Kernel.gets().chomp()
-  break if valid_num?(number2)  
-  prompt("Invalid number.")
-end
-
-prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-
-operator = Kernel.gets().chomp()
-
-result = case operator
-when '1'
-  number1.to_i() + number2.to_i()
-when '2'  
-  number1.to_i() - number2.to_i()
-when '3'
-  number1.to_i() * number2.to_i()
-when '4'
-  number1.to_f() / number2.to_f()
-end
-
-prompt("The result is #{result}.")
+prompt("Goodbye!")
