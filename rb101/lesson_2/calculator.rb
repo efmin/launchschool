80abee7c89ee493ae106e50796111921a45a7ebb
EFFY MIN <effyverse@gmail.com>
2020-10-28 13:02:36 -0400

    add a prompt method to display string messages

--- rb101/lesson_2/calculator.rb
+++ rb101/lesson_2/calculator.rb
@@ -1,20 +1,16 @@
-# ask the user for two numbers
-# ask the user for an operation to perform
-# perform the operation on the two numbers
-# output the result
+def prompt(message)
+  Kernel.puts ">> #{message}"
+end
 
-# answer = Kernel.gets()
-# Kernel.puts(answer)
+prompt("Welcome to Calculator!")
 
-Kernel.puts("Welcome to Calculator!")
-
-Kernel.puts("What's the first number?")
+prompt("What's the first number?")
 number1 = Kernel.gets().chomp()
 
-Kernel.puts("What's the second number?")
+prompt("What's the second number?")
 number2 = Kernel.gets().chomp()
 
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
 
 operator = Kernel.gets().chomp()
 
@@ -28,4 +24,4 @@ elsif
   result = number1.to_f() / number2.to_f()
 end
 
-puts "The result is #{result}."
+prompt("The result is #{result}.")
