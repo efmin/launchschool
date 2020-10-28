b6c175e55a78be303f3074ca0e96d104ac1e96ed
EFFY MIN <effyverse@gmail.com>
2020-10-28 15:51:36 -0400

    add validation method for user input numbers and create loop for each number

--- rb101/lesson_2/calculator.rb
+++ rb101/lesson_2/calculator.rb
@@ -2,26 +2,43 @@ def prompt(message)
   Kernel.puts ">> #{message}"
 end
 
+def valid_num?(n)
+  n.to_i != 0
+end
+
+
 prompt("Welcome to Calculator!")
 
-prompt("What's the first number?")
-number1 = Kernel.gets().chomp()
+number1 = nil
+number2 = nil
 
-prompt("What's the second number?")
-number2 = Kernel.gets().chomp()
+loop do
+  prompt("What's the first number?")
+  number1 = Kernel.gets().chomp()
+  break if valid_num?(number1)  
+  prompt("Invalid number.")
+end
+
+loop do
+  prompt("What's the second number?")
+  number2 = Kernel.gets().chomp()
+  break if valid_num?(number2)  
+  prompt("Invalid number.")
+end
 
 prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
 
 operator = Kernel.gets().chomp()
 
-if operator == "1"
-  result = number1.to_i() + number2.to_i()
-elsif operator == "2"
-  result = number1.to_i() - number2.to_i()
-elsif operator == "3"
-  result = number1.to_i() * number2.to_i()
-elsif
-  result = number1.to_f() / number2.to_f()
+result = case operator
+when '1'
+  number1.to_i() + number2.to_i()
+when '2'  
+  number1.to_i() - number2.to_i()
+when '3'
+  number1.to_i() * number2.to_i()
+when '4'
+  number1.to_f() / number2.to_f()
 end
 
 prompt("The result is #{result}.")
