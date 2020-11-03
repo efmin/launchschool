b58951d5144ee79221e2595f412c905e92b3f20c
EFFY MIN <effyverse@gmail.com>
2020-11-03 15:51:41 -0500

    method completed using only iterators and uniq

--- rb101/small_problems/easy_01/04.rb
+++ rb101/small_problems/easy_01/04.rb
@@ -2,13 +2,18 @@
 #   vehicles.tally.each {|car, count| puts "#{car} => #{count}"}
 # end
 
+---
+
 # redo assignment using brute force way instead of relying on rb abstraction:
 
-def count_occurrences(vehicles)
+# pseudo-code:
+
   # I need to create a hash that has a key of each unique value and a value of the total occurrence count of each key
   # to create the hash keys, I can use the uniq method on the input array to return an array of unique value 
   # to count the occurences, I need to loop through the original array and match each object with each hash key, add 1 count to the tally when it returns true
 
+
+def count_occurrences(vehicles)
   hash = {}
   vehicles.uniq.each do |vehicle|
     hash[vehicle] = 0
@@ -19,8 +24,8 @@ def count_occurrences(vehicles)
         hash[k] = v += 1
       end
     end
+    puts "#{k} => #{v}"
   end
-  puts hash
 end
 
 
