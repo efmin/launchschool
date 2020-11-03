c1cf16f3f3a0b6a2c110f137ede7f4d9e4138f09
EFFY MIN <effyverse@gmail.com>
2020-11-03 15:42:56 -0500

    write pseudo code to redo assignment using brute force code instead of relying on ruby abstraction

--- rb101/small_problems/easy_01/04.rb
+++ rb101/small_problems/easy_01/04.rb
@@ -1,6 +1,21 @@
+# def count_occurrences(vehicles)
+#   vehicles.tally.each {|car, count| puts "#{car} => #{count}"}
+# end
+
+# redo assignment using brute force way instead of relying on rb abstraction:
+
 def count_occurrences(vehicles)
-  vehicles.tally.each {|car, count| puts "#{car} => #{count}"}
+  # I need to create a hash that has a key of each unique value and a value of the total occurrence count of each key
+  # to create the hash keys, I can use the uniq method on the input array to return an array of unique value 
+  # to count the occurences, I need to loop through the original array and match each object with each hash key, add 1 count to the tally when it returns true
+
+  hash = {}
+  vehicles.uniq.each do |vehicle|
+    hash[vehicle] = 0
+  end
+  puts hash
 end
+
 
 vehicles = [
   'car', 'car', 'truck', 'car', 'SUV', 'truck',
