38c8888a50ca696dbe51ad99f034c95eebbc2c06
EFFY MIN <effyverse@gmail.com>
2020-11-04 14:45:15 -0500

    use 10DIME to process morgage_calculator question

---
+++ rb101/lesson_2/morgage_calculator.rb
@@ -0,0 +1,19 @@
+# 10DIME
+# Input: 
+# - loan_amount as integer
+# - APR as a float
+# - loan duration as integer
+#
+#
+# Output:
+# - 3 methods: 
+# 1. monthly interest rate method
+# 2. loan duration in months method
+# 3. monthly payment method
+#
+# Defintitions:
+# - APR = yearly interest rate
+#
+# Illustrations:
+# Methods:
+# Examples:
