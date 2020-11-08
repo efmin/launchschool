79b9e36db1a5174565bcff52f8e8c3f372fb2067
EFFY MIN <effyverse@gmail.com>
2020-11-08 11:22:20 -0500

    extract error message and welcome message to yaml config file for practice

--- rb101/lesson_2/morgage_calculator.rb
+++ rb101/lesson_2/morgage_calculator.rb
@@ -1,4 +1,8 @@
+require 'yaml'
 require 'pry'
+
+MESSAGES = YAML.load_file('morgage_calculator_messages.yml')
+binding.pry
 
 def prompt(message)
   puts ">> #{message}"
@@ -16,16 +20,16 @@ def monthly_payment_formula(loan_amount,
   (1 - (1 + monthly_interest_rate)**(-loan_duration_in_months)))
 end
 
-positive_value_error = 'Invalid input. Please enter a positive value.'
+# positive_value_error = 'Invalid input. Please enter a positive value.'
 
-prompt('Welcome to Morgage Calculator!')
+prompt(MESSAGES['welcome'])
 prompt('What is your loan amount?')
 
 loan_amount = ''
 loop do
   loan_amount = gets.chomp.to_f
   break if loan_amount > 0
-  prompt(positive_value_error)
+  prompt(MESSAGES['positive_value_error'])
 end
 
 
@@ -36,7 +40,7 @@ apr = ''
 loop do
   apr = gets.chomp.to_f
   break if apr > 0 
-  prompt(positive_value_error)
+  prompt(MESSAGES['positive_value_error'])
 end
 
 prompt("What is your loan duration in months?")
