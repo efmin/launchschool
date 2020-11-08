c8923393535ddd6034b4675d7d759ddbbbaf4238
EFFY MIN <effyverse@gmail.com>
2020-11-08 08:44:32 -0500

    separate the outline for morgage_calculator to its own vim file and write prompt method for user input

--- rb101/lesson_2/morgage_calculator.rb
+++ rb101/lesson_2/morgage_calculator.rb
@@ -1,25 +1,8 @@
-# 10DIME
-# Input: 
-# - loan_amount as integer
-# - APR as a float
-# - loan duration as integer
-#
-#
-# Output:
-# - 3 methods: 
-# 1. monthly interest rate method
-# 2. loan duration in months method
-# 3. monthly payment method
-#
-# Defintitions:
-# - APR = yearly interest rate
-#
-# Illustrations:
-# helper methods:
-# 1. convert APR (annual interest) to monthly interest
-# 2. check if loan duration is yearly, if so, convert to monthly
-#
-# I want to write a method whose output is my monthly payment (3.) and call these two helper methods inside that method so that I can convert the input values into the right ratios for the equation provided
-#
-# Methods:
-# Examples:
+def prompt(message)
+  puts ">> #{message}"
+end
+
+prompt("Welcome to Morgage Calculator. What is your loan amount?")
+loan_amount = gets.chomp
+# loan_amount is a string of an integer represented by P 
+
---
+++ rb101/lesson_2/morgage_calculator_outline.rb
@@ -0,0 +1,31 @@
+# 10DIME
+# Input: 
+# - loan_amount as integer
+# - APR as a float
+# - loan duration as integer
+#
+#
+# Output:
+# - 3 methods: 
+# 1. monthly interest rate method
+# 2. loan duration in months method
+# 3. monthly payment method
+#
+# Defintitions:
+# - APR = yearly interest rate
+#
+# Illustrations:
+# helper methods:
+# 1. convert APR (annual interest) to monthly interest
+# 2. check if loan duration is yearly, if so, convert to monthly
+#
+# I want to write a method whose output is my monthly payment (3.) and call these two helper methods inside that method so that I can convert the input values into the right ratios for the equation provided
+#
+# Methods:
+# Examples:
+
+# The provided formula:
+
+def monthly_payment
+  loan_amount * 
+end
