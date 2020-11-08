9342788b59140d2bea260588f61785bac4c45241
EFFY MIN <effyverse@gmail.com>
2020-11-08 08:54:19 -0500

    write method to calculate monthly interest rate based on APR and return value as an unrounded float. Print to screen.

--- rb101/lesson_2/morgage_calculator.rb
+++ rb101/lesson_2/morgage_calculator.rb
@@ -4,8 +4,18 @@ def prompt(message)
   puts ">> #{message}"
 end
 
+def monthly_interest_rate(apr)
+  apr.to_i / 12.0
+end
+
+
 prompt("Welcome to Morgage Calculator. What is your loan amount?")
 loan_amount = gets.chomp
 # loan_amount is a string of an integer represented by P 
+
+prompt("What is your APR? Please answer the integer value of your annual percentage rate.")
+apr = gets.chomp
+
+puts "You have entered #{apr} are your APR. We have calculated your monthly interest rate to be: #{monthly_interest_rate(apr)}"
 
 binding.pry
