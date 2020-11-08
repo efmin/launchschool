d79994d9ff6512850696260193d99c1aa5dced10
EFFY MIN <effyverse@gmail.com>
2020-11-08 10:15:46 -0500

    refactor error message for positive integer validation into a variable and add loop for validating user apr input

--- rb101/lesson_2/morgage_calculator.rb
+++ rb101/lesson_2/morgage_calculator.rb
@@ -12,20 +12,28 @@ def monthly_payment_formula(loan_amount, monthly_interest_rate, loan_duration_in
   loan_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-loan_duration_in_months)))
 end
 
+positive_value_error = 'Invalid input. Please enter a positive value.'
 
-prompt('Welcome to Morgage Calculator.')
+prompt('Welcome to Morgage Calculator!')
+prompt('What is your loan amount?')
 
 loan_amount = ''
 loop do
-  prompt('What is your loan amount?')
   loan_amount = gets.chomp.to_f
   break if loan_amount > 0
-  prompt('Invalid input. Please enter a positive loan amount.')
+  prompt(positive_value_error)
 end
 
 
-prompt("What is your APR? Please answer the integer value of your annual percentage rate.")
-apr = gets.chomp.to_i
+prompt('What is your annual interest rate?')
+prompt('Please answer with the integer value of your rate.')
+
+apr = ''
+loop do
+  apr = gets.chomp.to_f
+  break if apr > 0 
+  prompt(positive_value_error)
+end
 
 prompt("What is your loan duration in months?")
 loan_duration_in_months = gets.chomp.to_i
