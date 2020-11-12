690b4278973d75e280c0415b67e1ce81e54c7c25
EFFY MIN <effyverse@gmail.com>
2020-11-12 10:48:28 -0500

    add method for product calculation and printing

--- rb101/small_problems/easy_02/08.rb
+++ rb101/small_problems/easy_02/08.rb
@@ -10,18 +10,21 @@ def product(int)
   1.upto(int).reduce(:*)
 end
 
-def print_msg(int)
+def print_sum(int)
   prompt("The sum of the integers between 1 and #{int} is #{sum(int)}.")
+end
+
+def print_product(int)
+  prompt("The product of the integers between 1 and #{int} is #{product(int)}.")
 end
 
 prompt('Please enter an integer greater than 0:')
 number = gets.chomp.to_i
 
 prompt("Please enter 's' to compute the sum, 'p' to compute the product.")
-operation = gets.chomp
+operator = gets.chomp
 
-operation == 's' ? print_msg(number) : print_msg(number)
-
+operator == 's' ? print_sum(number) : print_product(number)
 
 
 
