c1a1259d431ac03d3c31a60f69172e55b0a746d4
EFFY MIN <effyverse@gmail.com>
2020-11-12 07:43:34 -0500

    rewrite method printing odd values using int#upto and enum#select

--- rb101/small_problems/easy_02/06.rb
+++ rb101/small_problems/easy_02/06.rb
@@ -2,4 +2,10 @@ def print_odd(input)
   input.each { |n| puts n if n.odd?}
 end
 
-print_odd(1..99)
+# print_odd(1..99)
+
+# Further exploration
+
+# 1.upto(99) {|n| puts n if n%2 == 1
+
+(1..99).select {|n| puts n unless n%2 == 0}
