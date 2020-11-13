362f170bc77a80044d603287aa7e5aedc70c186b
EFFY MIN <effyverse@gmail.com>
2020-11-13 08:45:08 -0500

    write 10DIME to process problem

---
+++ rb101/small_problems/easy_03/02.rb
@@ -0,0 +1,13 @@
+# 10DIME:
+
+# Input: two positive integers from user
+# Output: the result of performing each operation on the two int
+#
+# Definitions: The exercise asks for the following operations:
+# - addition, subtraction, product, quotient, remainder, and power
+#
+# Identify rules: N/A
+# Mental Model: I want a method that takes two integers, saves them into variables, and then outputs the result from operations
+# Examples/Edge Cases: - the question asks for the remainder not the modulus? 
+
+
