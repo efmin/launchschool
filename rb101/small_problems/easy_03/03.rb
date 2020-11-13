83f4419bdfd23d18898b329f84d922e15b0427bf
EFFY MIN <effyverse@gmail.com>
2020-11-13 09:22:51 -0500

    write pseudo-code for counting characters without white spaces

---
+++ rb101/small_problems/easy_03/03.rb
@@ -0,0 +1,17 @@
+# 10DIME
+
+# Input: a String from user input
+# Output: a String indicating the number  of characters of the user input String, not counting white spaces
+# Definitions: N/A
+# Identify Rules: not counting white spaces, display the count and the original input word in the output message
+# Mental Model: I want to save user input into a variable and split it into letters. Then I will to count all letters. I would like to try to write this as manually as possible.
+# Edge Cases: 
+# - empty input?
+
+# Pseudo-Code:
+# - prompt user, save string input
+# - split string into characters
+# - iterate through array of characters and see if there are any matches to a white space as: " "
+# - subtract white space count from count of letters and output the result in a string 
+
+
