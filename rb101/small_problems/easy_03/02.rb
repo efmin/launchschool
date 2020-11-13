3582405940aadd277c12160c45f3867e93b1c248
EFFY MIN <effyverse@gmail.com>
2020-11-13 09:01:51 -0500

    write method to calculate binary operations on user input integers

--- rb101/small_problems/easy_03/02.rb
+++ rb101/small_problems/easy_03/02.rb
@@ -10,4 +10,27 @@
 # Mental Model: I want a method that takes two integers, saves them into variables, and then outputs the result from operations
 # Examples/Edge Cases: - the question asks for the remainder not the modulus? 
 
+# Pseudo-Code:
+# - user prompt for two integers
+# - save integers
+# - perform calculations and display in same line
+
+def prompt(msg)
+  puts "==> #{msg}"
+end
+
+prompt('Enter the first number:')
+num1 = gets.chomp.to_i 
+
+prompt('Enter the second number:')
+num2 = gets.chomp.to_i
+
+prompt("#{num1} + #{num2} = #{num1 + num2}")
+prompt("#{num1} - #{num2} = #{num1 - num2}")
+prompt("#{num1} * #{num2} = #{num1 * num2}")
+prompt("#{num1} / #{num2} = #{num1 / num2}")
+prompt("#{num1} + #{num2} = #{num1.remainder(num2)}")
+prompt("#{num1} * #{num2} = #{num1 ** num2}")
+
+
 
