a19beb578bd687f93da285092c6f806747774987
EFFY MIN <effyverse@gmail.com>
2020-11-17 14:05:56 -0500

    initalize a CONTANT variable SCORES that references a hash to track user points

--- rb101/lesson_2/rockpaperscissorslizardspock-2.rb
+++ rb101/lesson_2/rockpaperscissorslizardspock-2.rb
@@ -10,6 +10,13 @@ WINNING_COMBOS = {
   'spock' => ['rock', 'scissors']
 }
 
+SCORES = {
+  'user_score' => 0,
+  'computer_score' => 0
+}
+
+
+
 def prompt(msg)
   puts ">> #{msg}"
 end
@@ -28,24 +35,19 @@ def display_result(choice, computer_choice)
   end
 end
 
-# def increment_score(choice, computer_choice, user_score, computer_score)
-#   if win?(choice, computer_choice)  
-#     user_score += 1
-#   elsif win?(computer_choice, choice)
-#     computer_score += 1
-#   else
-#     prompt("It's tie!")
-#   end
-# end
+def increment_score(choice, computer_choice)
+  if win?(choice, computer_choice)  
+    SCORES['user_score'] += 1
+  elsif win?(computer_choice, choice)
+    SCORES['computer_score'] += 1
+  end
+end
 
 
 
 prompt('Welcome!')
 
-user_score = 0
-computer_score = 0
-
-# until user_score == 5 || computer_score == 5 
+# until 'user_score' == 5 || 'computer_score' == 5 
   loop do
     prompt('Please pick rock, paper, scissors, spock, or lizard:')
 
@@ -60,27 +62,9 @@ computer_score = 0
     prompt("You picked #{choice} and Computer picked #{computer_choice}.")
 
     display_result(choice, computer_choice)
-    
-    # if win?(choice, computer_choice)  
-    #   prompt('You won!')
-    # elsif win?(computer_choice, choice)
-    #   prompt('Computer won!')
-    # else
-    #   prompt("It's tie!")
-    # end
-    
-    # puts computer_choice
-    # puts choice 
-    
-    # increment score
-    
-    if win?(choice, computer_choice)
-      user_score += 1
-    elsif win?(computer_choice, choice)
-      computer_score += 1
-    end
-
-    puts "The score is now: User - #{user_score} VS. Computer - #{computer_score}!"
+   
+    increment_score(choice, computer_choice) 
+    puts "The score is now: \nUser - #{SCORES['user_score']} \nComputer - #{SCORES['computer_score']}!"
 
     prompt('Would you like to play again?')
     answer = gets.chomp
