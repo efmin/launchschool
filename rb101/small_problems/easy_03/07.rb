fa40fc318c732f3d9555dc6446170d0bc6c7fae5
EFFY MIN <effyverse@gmail.com>
2020-11-18 12:18:22 -0500

    write method that returns every other el starting with first el

---
+++ rb101/small_problems/easy_03/07.rb
@@ -0,0 +1,11 @@
+def oddities(arr)
+  result = []
+  arr.each_with_index {|obj, i| result << obj if i.even?}
+  result
+end
+
+puts oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+puts oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+puts oddities(['abc', 'def']) == ['abc']
+puts oddities([123]) == [123]
+puts oddities([]) == []
